6ed6fcd9bf8deeb61cb9b2cf5cf224a318d9f945
Diegox0410 <diegovelasquez7u8@gmail.com>
2026-06-14 11:41:57 -0500

    fix firebase import case sensitive

--- src/Pages/Publico.jsx
+++ src/Pages/Publico.jsx
@@ -6,7 +6,7 @@ import {
   onSnapshot,
 } from "firebase/firestore";
 
-import { db } from "../services/firebase";
+import { db } from "../services/Firebase";
 
 export default function Publico() {
   const [participantes, setParticipantes] =
